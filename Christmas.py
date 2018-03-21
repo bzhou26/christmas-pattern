bf547a65dca2615017564a679bc68f0459cbef6d
Bo Zhou <bzhou2@ualberta.ca>
2018-03-21 17:43:24 -0600

    initial repo

---
+++ Christmas.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+class Christmas:
+    """
+    This is a class to print a christmas tree or christmas X
+    """
+
+    def __init__(self):
+        pass
+
+    def print_tree(self, height):
+        pass
+
+    def print_x(self, height):
+        pass
+
+    def __print_line_pattern(self, back_pattern, front_pattern, length):
+        pass
+
+if __name__ == "__main__":
+    pass
